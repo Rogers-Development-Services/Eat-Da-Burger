dea8b82a1b34285e31871ddb02370f6b47dc9804
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-09-30 16:35:07 -0700

    set up server.js

--- models/burgers.js
+++ models/burgers.js
@@ -1,3 +1,21 @@
 const orm = require("../config/orm");
 
+var burger = {
+    selectAll: function (callback) {
+        orm.selectAll("burgers", function (res) {
+          callback(res);
+        });
+      },
+      create: function (cols, vals, callback) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+          callback(res);
+        });
+      },
+      update: function (objColVals, condition, callback) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+          callback(res);
+        });
+      },
+}
+
 module.exports = burger;

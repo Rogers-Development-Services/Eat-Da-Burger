66769ddb409335347cd2336b686bb0d5c9219eba
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-10-03 16:13:05 -0700

    Added JawsDB_URL to connection.js

--- config/connection.js
+++ config/connection.js
@@ -11,6 +11,17 @@ const connectionConfig = {
 
 var connection = mysql.createConnection(connectionConfig);
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
+
 connection.connect(function (err) {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
